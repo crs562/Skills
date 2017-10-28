2d098cae24f8ebf5e8b5e22ced1efc23e9f1f34d
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-10-28 01:03:06 -0400

    Add files via upload

---
+++ Java/ThinkJava/CH2/Data.java
@@ -0,0 +1,24 @@
+public class Data // class
+{
+    public static void main(String[] args) // method
+    {
+        String day = "Friday", month = "September";
+        int date = 29, year = 2017;
+        System.out.println("American format:");
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(month);
+        System.out.print(" ");
+        System.out.print(date);
+        System.out.print(", ");
+        System.out.println(year);
+        System.out.println("European format:");
+        System.out.print(day);
+        System.out.print(", ");
+        System.out.print(date);
+        System.out.print(" ");
+        System.out.print(month);
+        System.out.print(", ");
+        System.out.println(year);
+   }
+}
