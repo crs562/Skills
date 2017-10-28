f297f66ca1666cda66be7e56cad4a00afa62d680
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-10-28 00:52:49 -0400

    Add files via upload

---
+++ Java/ThinkJava/Ch1/Goodbye.java
@@ -0,0 +1,14 @@
+/**
+ * Example program that demonstrates print vs println.
+ */
+public class Goodbye
+{
+    /**
+     * Prints a greetin
+     */
+    public static void main(String[] args)
+    {
+        System.out.print("Goodbye, "); // note the space
+        System.out.println("cruel world");
+    }
+}
