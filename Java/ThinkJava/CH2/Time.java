9b08079810a4c76f6c34da15165475812dfdddf2
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-10-28 01:05:00 -0400

    Add files via upload

---
+++ Java/ThinkJava/CH2/Time.java
@@ -0,0 +1,15 @@
+public class Time // class
+{
+    public static void main(String[] args) // method
+    {
+        int hour = 14, minute = 40, second = 20;
+        System.out.print("Total number of seconds from midnight: ");
+        int total_second;
+        total_second = hour*3600 + minute*60 + second;
+        System.out.println(total_second);
+        System.out.print("Total number of seconds remaining in the day: ");
+        System.out.println(24*3600 - total_second);
+        System.out.print("Percentage of the day that has passed: ");
+        System.out.println(total_second*100.0/(24.0*3600.0));
+   }
+}
