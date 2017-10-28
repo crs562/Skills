00a8126a93e6ab23a29120278c0b6c1624c24473
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-10-28 00:55:34 -0400

    Update Hello.java

--- Java/ThinkJava/Ch1/Hello.java
+++ Java/ThinkJava/Ch1/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     // generate some simple output
     System.out.println("Hello, World!"); // first line
     System.out.println("How are you?"); // another line
+    System.out.print("Hello!\nHow are you doing?\n");
+    System.out.println("She said \"Hello!\" to me.");
   }
 }
