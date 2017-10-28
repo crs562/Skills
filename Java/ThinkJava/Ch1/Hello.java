dff155ec5f1055aa5bbac222d19af7f8600f832d
Chaitanya Shah <acs.chaitanya35@gmail.com>
2017-10-28 00:51:21 -0400

    Update Hello.java

--- Java/ThinkJava/Ch1/Hello.java
+++ Java/ThinkJava/Ch1/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
   public static void main(String[] args) {
     // generate some simple output
-    System.out.println("Hello, World!");
+    System.out.println("Hello, World!"); // first line
+    System.out.println("How are you?"); // another line
   }
 }
